b9955570b348199bbcfe4b51953baed21d16f6a2
Tika~ <barasatyaradi@gmail.com>
2023-10-01 23:54:36 +0700

    Fix Navbar

--- resources/views/dashboardPemilik.blade.php
+++ resources/views/dashboardPemilik.blade.php
@@ -20,108 +20,93 @@
     <!-- Font -->
 </head>
 
-<body>
-    <main>
-        <div id="wrapper">
-            <aside id="sidebar-wrapper">
-                <button class="float-end align-text-top pe-1" id="close-sidebar">
-                    <i class="bi bi-x" style="color: black"></i>
-                </button>
+<body style="width: 100%">
+    <header>
+        <!-- Navbar -->
+        <div class="d-inline">
+            <div id="wrapper">
+                <aside id="sidebar-wrapper">
+                    <button class="float-end align-text-top pe-1" id="close-sidebar">
+                        <i class="bi bi-x" style="color: black"></i>
+                    </button>
 
-                <div class="sidebar-brand p-0 mt-2">
-                    <div class="m-0 p-0 d-flex align-items-center">
-                        <img src="assets/navbar/logo.png" alt="Logo" width="50" height="50"
-                            class="d-inline-block ms-1 float-start">
-                        <h2 class="m-0 p-0 ms-2 align-middle" style="color: BLACK"> MONEYTRASH</h2>
-                    </div>
-
-                </div>
-
-                <ul class="sidebar-nav">
-                    <li class="active">
-                        <a href="#"><i class="bi bi-house"></i> <span id="icon-home"> Home</span></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="bi bi-activity"></i><span id="icon-pembayaran">
-                                Pembayaran</span></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="bi bi-clock-history"></i><span id="icon-riwayat"> Riwayat</span></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="bi bi-person"></i><span id="icon-pengaturan"> Pengaturan</span></a>
-                    </li>
-                </ul>
-            </aside>
-
-            <div id="navbar-wrapper">
-                <nav class="navbar navbar-inverse">
-                    <div class="container-fluid">
-                        <div class="navbar-header">
-                            <a href="#" class="navbar-brand" id="sidebar-toggle"><i class="bi bi-list"></i></a>
+                    <div class="sidebar-brand p-0 mt-2">
+                        <div class="m-0 p-0 d-flex align-items-center">
+                            <img src="assets/navbar/logo.png" alt="Logo" width="50" height="50"
+                                class="d-inline-block ms-1 float-start">
+                            <h2 class="m-0 p-0 ms-2 align-middle" style="color: BLACK"> MONEYTRASH</h2>
                         </div>
+
                     </div>
-                </nav>
+
+                    <ul class="sidebar-nav">
+                        <li class="active">
+                            <a href="#"><i class="bi bi-house"></i> <span id="icon-home"> Home</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="bi bi-activity"></i><span id="icon-pembayaran">
+                                    Pembayaran</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="bi bi-clock-history"></i><span id="icon-riwayat">
+                                    Riwayat</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="bi bi-person"></i><span id="icon-pengaturan">
+                                    Pengaturan</span></a>
+                        </li>
+                    </ul>
+                </aside>
+
+                <div id="navbar-wrapper">
+                    <nav class="navbar navbar-inverse">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                                <a href="#" class="navbar-brand" id="sidebar-toggle"><i
+                                        class="bi bi-list"></i></a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
             </div>
+        </div>
+        <!-- Navbar -->
+    </header>
 
-            {{-- Jenis Layanan --}}
-            <section id="content-wrapper">
-                <div class="row">
-                    <div class="col-lg-12 mb-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <h1>Jenis Layanan</h1>
-                                    </div>
-                                    <div class="row m-0 p-0 border">
-                                        <div class="col-6">
-                                            <div class="card">
-                                                <div class="card-body">
-                                                    <h5 class="card-title">Special title treatment</h5>
-                                                    <p class="card-text">With supporting text below as a natural
-                                                        lead-in
-                                                        to
-                                                        additional content.</p>
-                                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-6">
-                                            <div class="card">
-                                                <div class="card-body">
-                                                    <h5 class="card-title">Special title treatment</h5>
-                                                    <p class="card-text">With supporting text below as a natural
-                                                        lead-in
-                                                        to
-                                                        additional content.</p>
-                                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+    <main>
+        <div class="me-0" id="content-wrapper">
+            <div class="row">
+                <div class="col-lg-12 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <h1>Jenis Layanan</h1>
+                                <div class="col-md-6 pb-2 pb-sm-2 pb-md-0 pb-lg-0 pb-xl-0 d-flex justify-content-center">
+                                    <button class="btn btn-warning btn-block ambil">Ambil Di Rumah</button>
+                                </div>
+                                <div class="col-md-6">
+                                    <button class="btn btn-success btn-block antar d-flex justify-content-center">Antar Sendiri</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                {{-- Maps --}}
-                <div class="row">
-                    <div class="col-lg-12 mb-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <h1>Peta</h1>
-                                </div>
-                                <div class="row m-0 p-0 border">
-                                    <div id="map"></div>
-                                </div>
+            </div>
+            {{-- Maps --}}
+            <div class="row">
+                <div class="col-lg-12 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                <h1>Peta</h1>
+                            </div>
+                            <div class="row m-0 p-0 border">
+                                <div id="map"></div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
-
+            </div>
         </div>
     </main>
     <footer>
