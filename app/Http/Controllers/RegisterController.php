bfde73e6888bdffa0b092c56308f24c87e5c1455
unknown <barasatyaradi@gmail.com>
2023-11-02 16:00:22 +0700

    Update OAuth

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\UserGoogleModel;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use Google;
 
 class RegisterController extends Controller
 {
@@ -16,6 +17,20 @@ class RegisterController extends Controller
 
     public function auth()
     {
+        require('./vendor/autoload.php');
+
+        # Add your client ID and Secret
+        $client_id = "233436046340-3g8k1tar7j9evj6btbbgb7l9ak4637hn.apps.googleusercontent.com";
+        $client_secret = "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";
+
+        $client = new Google\Client();
+        $client->setClientId($client_id);
+        $client->setClientSecret($client_secret);
+
+        # redirection location is the path to login.php
+        $redirect_uri = './login';
+        $client->setRedirectUri($redirect_uri);
+
         if (isset($_GET['access_token'])) {
             $code = $_GET['access_token'];
 
