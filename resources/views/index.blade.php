04fc4432c0f73de04e6121cc0f3d92e3c378498d
unknown <barasatyaradi@gmail.com>
2023-11-02 10:20:04 +0700

    Update Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="/assets/styles/index/plugins.min.css">
     <!-- Style CSS -->
     <link rel="stylesheet" href="/assets/styles/index/app.css">
+    
     <!-- Your CSS -->
     <link rel="stylesheet" href="/assets/styles/index/custom.css">
 
@@ -24,20 +25,40 @@
     <main class="main">
         <!-- =========== Start of  Navigation ============ -->
 
-        <nav class="navbar navigation__transparent  navbar-expand-lg navbar-light position-absolute">
+        <nav class="navbar navigation__transparent navbar-expand-lg navbar-light position-absolute">
             <div class="container position-relative">
 
-                <a class="navbar-brand" href="/">
-                    <img src="/assets/navbar/logo-long-dark.png" alt="" class="img-fluid" width="220px">
+                <a class="navbar-brand" href="index">
+                    <img src="/assets/navbar/logo-long-dark.png" alt="" class="img-fluid" width="230px">
                 </a>
+                <!-- navbar-brand end -->
 
+                <button class="navbar-toggler d-block d-sm-none" type="button" data-toggle="collapse" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon d-block d-sm-none"></span>
+                </button>
+                <!-- navbar-toggler end -->
+
+                <div class="navbar-inner ml-auto mr-lg-50 d-block d-sm-none" id="navbar-inner">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <!-- navbar-toggler end -->
+
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="login">Login!</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- collapse end -->
                 <div class="navbar-btn mr-60 mr-lg-0">
-                    <a href="login"
-                        class="btn btn-outline btn-outline--sm border-color-white btn-color--white btn-outline-hover--white btn-hover">Login !</a>
+                    <a href="#"
+                        class="btn btn-outline btn-outline--sm border-color-white btn-color--white btn-outline-hover--white btn-hover">Login</a>
                 </div>
             </div>
         </nav>
         <!-- =========== End of  Navigation ============ -->
+
 
         <!-- =========== End of  Hero ============ -->
         <section class="hero hero--v1 hero--full overflow-hidden d-flex align-items-center">
@@ -50,7 +71,8 @@
                 <div class="row text-center text-lg-left pb-70 pb-lg-0">
                     <div class="col-12 col-lg-6">
                         <div class="hero-content mb-35">
-                            <h1 class="color-white mb-15" style="font-weight: bold">MoneyTrash<br>Trash Me! It's Work </h1>
+                            <h1 class="color-white mb-15" style="font-weight: bold">MoneyTrash<br>Trash Me! It's Work
+                            </h1>
                             <p class="h6-font color-white">No one cares about products. People care about ideas. Is a
                                 product an idea? Noup. Is a brand? A good one is.
                             </p>
@@ -60,7 +82,8 @@
                             For Free</a>
                         <div class="col-12 col-lg-5 hero-img pos-abs-bottom-right p-5 m-5 d-lg-none">
                             <span>
-                                <img src="/assets/navbar/logo.png" alt="hero-image" class="img-fluid p-5 m-5" width="320px">
+                                <img src="/assets/navbar/logo.png" alt="hero-image" class="img-fluid p-5 m-5"
+                                    width="320px">
                             </span>
                         </div>
                     </div>
@@ -135,16 +158,14 @@
                     <div class="col-12 col-lg-7 mx-auto">
                         <div class="text-center">
                             <a class="mb-25" href="index.html">
-                                <img src="img/brand-logo-color.png" alt="">
+                                <img src="/assets/navbar/logo.png" alt="" width="50px">
                             </a>
-                            <p class="font-size--16 text-color--400 mb-55">Sitting proudly atop M3565 is the two-storey
-                                penthouse. The master bedroom suite is phenomenally spacious and opens out to a
-                                breathtaking plunge pool. </p>
+                            <p class="font-size--16 text-color--400 mb-55">MoneyTrash! <br> Salah satu project RPL yang didirikan oleh Team RPL Moneytrash</p>
 
                             <div class="widget widget-nav">
                                 <ul>
                                     <li>
-                                        <a href="#">Affiliate program</a>
+                                        <a href="#">Kantor</a>
                                     </li>
                                     <li>
                                         <a href="#">Terms & Conditions</a>
@@ -153,15 +174,14 @@
                                         <a href="#">Privacy Policy</a>
                                     </li>
                                     <li>
-                                        <a href="#">Refund Policy</a>
+                                        <a href="#">Media Social</a>
                                     </li>
                                 </ul>
                             </div>
                             <!-- widget end -->
                             <span
                                 class="copyright text-color--400 text-dark-400 d-flex align-items-center justify-content-center">
-                                &copy;2018 vApp, Design with <i class="nc-icon nc-heart-2 color-orange"></i> <a
-                                    href="#">Team Dhrubok</a>
+                                &copy;2023 Team RPL
                             </span>
                         </div>
                     </div>
