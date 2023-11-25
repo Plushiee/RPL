07254611c8bdef8162adba9f2f2a9a2d6c4a4040
Tika~ <barasatyaradi@gmail.com>
2023-11-25 22:35:18 +0700

    bank terima

--- resources/views/terima-bank.blade.php
+++ resources/views/terima-bank.blade.php
@@ -62,6 +62,126 @@
                                             <div class="col-12 col-sm-10 d-flex align-items-center">
                                                 <div class="row">
                                                     <div class="col-12">
+                                                        <h4 class="m-0 p-0">Sampah
+                                                            {{ $transaksi->jenisSampah }}
+                                                        </h4>
+                                                        <p class="m-0 p-0">
+                                                            {{ \Carbon\Carbon::parse($transaksi->created_at)->format('d-m-Y') }}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-sm-2 d-flex align-items-center justify-content-end">
+                                                @if (!$transaksi->approved)
+                                                    <span class="badge badge-warning"> &nbsp;Belum
+                                                        Di Approved&nbsp; </span>
+                                                @else
+                                                    <span class="badge badge-warning"> &nbsp;Sudah
+                                                        Approved&nbsp; </span>
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div
+                                                class="col-md-2 text-center d-flex justify-content-center align-items-center">
+                                                <h5 class="text-muted mb-0 mt-0 pt-0">
+                                                    {{ $transaksi->name }}</h5>
+                                            </div>
+                                            <div
+                                                class="col-md-2 text-center d-flex justify-content-center align-items-center">
+                                                <h5 class="text-muted mb-0 mt-0 pt-0">{{ $transaksi->nama }}
+                                                </h5>
+                                            </div>
+                                            <div
+                                                class="col-md-2 text-center d-flex justify-content-center align-items-center">
+                                                <p class="text-muted mb-0 small">{{ $transaksi->nomor }}</p>
+                                            </div>
+                                            <div
+                                                class="col-md-2 text-center d-flex justify-content-center align-items-center">
+                                                <p class="text-muted mb-0 small">
+                                                    <b>Catatan Tambahan : </b><br>
+                                                    @if ($transaksi->catatanTambahan == null)
+                                                        -
+                                                    @else
+                                                        {{ $transaksi->catatanTambahan }}
+                                                    @endif
+                                                </p>
+                                            </div>
+                                            <div
+                                                class="col-md-2 text-center d-flex justify-content-center align-items-center">
+                                                <p class="text-muted mb-0 small">
+                                                    <b>Berat :</b><br>{{ $transaksi->berat }} Kg
+                                                </p>
+                                            </div>
+                                            <div
+                                                class="col-md-2 mt-3 mt-sm-2 mt-md-0 text-center d-flex justify-content-center align-items-center">
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <form action="/pengambil/dashboard/ambil" method="POST">
+                                                            @csrf
+                                                            <input type="hidden" name="id_transaksi"
+                                                                value="{{ $transaksi->id }}">
+                                                            @if (
+                                                                $transaksi->diterima == false &&
+                                                                    $transaksi->terbayar == false &&
+                                                                    $transaksi->approved == false &&
+                                                                    $transaksi->terambil == false)
+                                                                <input type="hidden" name="aksi" value="diterima">
+                                                                <button type="submit" id="terima"
+                                                                    class="btn btn-info btn-block mb-2 terima">
+                                                                    Terima Pesanan
+                                                                </button>
+                                                            @endif
+                                                        </form>
+                                                    </div>
+                                                    <div class="col-md-12">
+                                                        <button type="button"
+                                                            class="btn btn-info small mt-2 mt-sm-2 mt-md-0 btn-block informasi"
+                                                            id="informasi" data-id="{{ $transaksi->idPemilik }}"
+                                                            data-bukti="{{ $transaksi->bukti }}"
+                                                            data-banksampah="{{ $transaksi->name }}"
+                                                            data-alamat="{{ $transaksi->alamat }} ({{ $transaksi->catatan }}), {{ $transaksi->kecamatan }}, {{ $transaksi->kota }}, {{ $transaksi->provinsi }}, {{ $transaksi->kodePos }}"
+                                                            data-lang="{{ $transaksi->lang }}"
+                                                            data-long="{{ $transaksi->long }}">Informasi
+                                                            Lainnya</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+                                        </div>
+                                        <hr class="mb-4" style="background-color: #e0e0e0; opacity: 1;">
+                                        <div class="row d-flex align-items-center">
+                                            <div class="col-md-2">
+                                                <h5 class="text-muted mb-0 mt-0">Track Order</h5>
+                                            </div>
+                                            <div class="col-md-10">
+                                                <div class="progress" style="height: 6px; border-radius: 16px;">
+                                                    <div class="progress-bar" role="progressbar"
+                                                        style="
+                                                    @if ($transaksi->approved === true && $transaksi->terkirim === false) width: 50%;
+                                                    @elseif ($transaksi->approved === true && $transaksi->terkirim === true)
+                                                    width: 100%;
+                                                    @else
+                                                    width: 0%; @endif
+                                                     border-radius: 16px; background-color: #18be55;"
+                                                        aria-valuemin="0" aria-valuemax="100">
+                                                    </div>
+                                                </div>
+                                                <div class="d-flex justify-content-around mb-1">
+                                                    <p class="text-muted mt-1 mb-0 small ms-xl-5">Diterima</p>
+                                                    <p class="text-muted mt-1 mb-0 small ms-xl-5">Terkirim</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {{-- <div class="card shadow-0 border mb-4">
+                                    <div class="card-body pt-2">
+                                        <div class="row mb-3">
+                                            <div class="col-12 col-sm-10 d-flex align-items-center">
+                                                <div class="row">
+                                                    <div class="col-12">
                                                         <h4 class="m-0 p-0">Sampah {{ $transaksi->jenisSampah }}
                                                         </h4>
                                                         <p class="m-0 p-0">
@@ -125,11 +245,7 @@
                                                             @csrf
                                                             <input type="hidden" name="id_transaksi"
                                                                 value="{{ $transaksi->id }}">
-                                                            @if (
-                                                                $transaksi->diterima == false &&
-                                                                    $transaksi->terbayar == false &&
-                                                                    $transaksi->approved == false &&
-                                                                    $transaksi->terambil == false)
+                                                            @if ($transaksi->diterima == false && $transaksi->terbayar == false && $transaksi->approved == false && $transaksi->terambil == false)
                                                                 <input type="hidden" name="aksi" value="diterima">
                                                                 <button type="submit" id="terima"
                                                                     class="btn btn-info btn-block mb-2 terima">
@@ -216,7 +332,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </div> --}}
                             @endif
                         @endforeach
                     </div>
